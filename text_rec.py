ba9d7d177fa9b5175796dca7d6ba10ec86e54d67
SNAS <mukherjeesaranya722@gmail.com>
2025-04-25 11:48:16 +0530

    added new text recognition test block

--- text_rec.py
+++ text_rec.py
@@ -3,7 +3,11 @@ import numpy as np
 import colours
 
 img = cv2.imread("1st_ref.png", cv2.IMREAD_COLOR)
-certif = cv2.imread("results/Kalo the kalo nag.jpg", cv2.IMREAD_COLOR)
+certif = cv2.imread("results/Soham Kanti Paira.jpg", cv2.IMREAD_COLOR)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv, colours.c1, colours.c2)
 
+cv2.imshow("certif", certif)
+cv2.imshow("mask", mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
